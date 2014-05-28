7539c0f62deb69c528e8811df1012c8c547ee305
Alejandro Samboy <alejandro.samboy+github@gmail.com>
2014-05-28 00:23:52 -0400

    jS Functionality added

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+	var cards = $(".cardwrap").sort(function(){return (Math.round(Math.random()) - 0.5);});
+
+	$(".cardwrap").remove();
+
+	$(".wrapper").append(cards);
+
+	$(".card").click(function(){
+		if ($(this).attr('class') != "flipped" && $(this).attr('class') != "match") {
+			
+
+			if ($(".flipped").get().length==2) {
+				$(".flipped").removeClass("flipped");
+			}
+
+			if ( $(this).children(".back").attr('class') == $(".flipped").children(".back").attr('class') ) {
+				$(this).addClass("flipped");
+				$(".flipped").addClass("match");
+			}
+
+			$(this).addClass("flipped");
+		}
+	});
+});
+
