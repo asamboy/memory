794f935acbf4f3a4f0fdcf7eef6cdb76f6badcda
Alejandro Samboy <alejandro.samboy+github@gmail.com>
2014-05-28 23:30:22 -0400

    Memory v1.0

--- js/script.js
+++ js/script.js
@@ -1,26 +1,42 @@
 $(document).ready(function(){
 
-	var cards = $(".cardwrap").sort(function(){return (Math.round(Math.random()) - 0.5);});
-
-	$(".cardwrap").remove();
-
-	$(".wrapper").append(cards);
+	randomize();
 
 	$(".card").click(function(){
-		if ($(this).attr('class') != "card flipped" && $(this).attr('class') != "card flipped match") {
+		if ($(this).attr("class") != "card flipped" && $(this).attr("class") != "card flipped match") {
 			
 
 			if ($(".flipped").get().length==2) {
 				$(".flipped").removeClass("flipped");
 			}
 
-			if ( $(this).children(".back").attr('class') == $(".flipped").children(".back").attr('class') ) {
+			if ( $(this).children(".back").attr("class") == $(".flipped").children(".back").attr("class") ) {
 				$(this).addClass("flipped");
 				$(".flipped").addClass("match");
 			}
 
 			$(this).addClass("flipped");
+
+			if ($(".match").get().length == 8) {
+				$("#modal").foundation("reveal", "open");
+				
+			}
 		}
 	});
+
+	
+});
+
+
+function randomize() {
+	var cards = $(".cardwrap").sort(function(){return (Math.round(Math.random()) - 0.5);});
+	$(".cardwrap").remove();
+	$(".wrapper").append(cards);
+}
+
+$(".again").click(function(){
+	$(".card").removeClass("flipped").removeClass("match");
+	randomize();
+	$('#modal').foundation('reveal', 'close');
 });
 
