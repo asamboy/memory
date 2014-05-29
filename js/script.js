6b481ff6ae8989ad361d06ee36c7211ce1aee2e9
Alejandro Samboy <alejandro.samboy+github@gmail.com>
2014-05-28 23:55:53 -0400

    Replay bug correction

--- js/script.js
+++ js/script.js
@@ -1,42 +1,37 @@
-$(document).ready(function(){
-
-	randomize();
-
-	$(".card").click(function(){
-		if ($(this).attr("class") != "card flipped" && $(this).attr("class") != "card flipped match") {
-			
-
-			if ($(".flipped").get().length==2) {
-				$(".flipped").removeClass("flipped");
-			}
-
-			if ( $(this).children(".back").attr("class") == $(".flipped").children(".back").attr("class") ) {
-				$(this).addClass("flipped");
-				$(".flipped").addClass("match");
-			}
-
-			$(this).addClass("flipped");
-
-			if ($(".match").get().length == 8) {
-				$("#modal").foundation("reveal", "open");
-				
-			}
-		}
-	});
-
-	
-});
-
-
 function randomize() {
 	var cards = $(".cardwrap").sort(function(){return (Math.round(Math.random()) - 0.5);});
 	$(".cardwrap").remove();
 	$(".wrapper").append(cards);
 }
 
+randomize();
+
+$("body").on("click", ".card", function(){
+	if ($(this).attr("class") != "card flipped" && $(this).attr("class") != "card flipped match") {
+		
+
+		if ($(".flipped").get().length==2) {
+			$(".flipped").removeClass("flipped");
+		}
+
+		if ( $(this).children(".back").attr("class") == $(".flipped").children(".back").attr("class") ) {
+			$(this).addClass("flipped");
+			$(".flipped").addClass("match");
+		}
+
+		$(this).addClass("flipped");
+
+		if ($(".match").get().length == 8) {
+			$("#modal").foundation("reveal", "open");
+			
+		}
+	}
+});
+
 $(".again").click(function(){
-	$(".card").removeClass("flipped").removeClass("match");
+	$(".card").removeClass("flipped");
+	$(".card").removeClass("match");
 	randomize();
-	$('#modal').foundation('reveal', 'close');
+	$("#modal").foundation("reveal", "close");
 });
 
